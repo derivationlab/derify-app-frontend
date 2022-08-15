8e643836c917de778a59e70db7974a31da1f2348
derivationstaff002 <38176557+sunmoon1992@users.noreply.github.com>
2022-08-15 22:01:58 +0800

    no message

--- src/pages/web/Trade/hooks.ts
+++ src/pages/web/Trade/hooks.ts
@@ -5,45 +5,49 @@ import BN from 'bignumber.js'
 import { nonBigNumberInterception } from '@/utils/tools'
 
 export const usePairsExtDataPoll = () => {
-  useSWR('pairs-extra-data', async () => {
-    const { data } = await getEventsData()
-    if (data.length) {
-      return data.map(
-        ({
-           token,
-           longUsdPmrRate,
-           longDrfPmrRate,
-           shortDrfPmrRate,
-           shortUsdPmrRate,
-           price_change_rate,
-           ...rest
-         }: Record<string, any>) => {
-          const long = new BN(longDrfPmrRate).plus(longUsdPmrRate)
-          const longPmrRate = nonBigNumberInterception(String(long))
+  useSWR(
+    'pairs-extra-data',
+    async () => {
+      const { data } = await getEventsData()
+      if (data.length) {
+        return data.map(
+          ({
+            token,
+            longUsdPmrRate,
+            longDrfPmrRate,
+            shortDrfPmrRate,
+            shortUsdPmrRate,
+            price_change_rate,
+            ...rest
+          }: Record<string, any>) => {
+            const long = new BN(longDrfPmrRate).plus(longUsdPmrRate)
+            const longPmrRate = nonBigNumberInterception(String(long))
 
-          const short = new BN(shortDrfPmrRate).plus(shortUsdPmrRate)
-          const shortPmrRate = nonBigNumberInterception(String(short))
+            const short = new BN(shortDrfPmrRate).plus(shortUsdPmrRate)
+            const shortPmrRate = nonBigNumberInterception(String(short))
 
-          const price = String(price_change_rate)
-          const changeRate = nonBigNumberInterception(price, 4)
+            const price = String(price_change_rate)
+            const changeRate = nonBigNumberInterception(price, 4)
 
-          const apyMax = Math.max(Number(longPmrRate), Number(shortPmrRate))
+            const apyMax = Math.max(Number(longPmrRate), Number(shortPmrRate))
 
-          return {
-            ...rest,
-            apy: apyMax,
-            token: token.toLowerCase(),
-            longPmrRate,
-            shortPmrRate,
-            price_change_rate: changeRate
+            return {
+              ...rest,
+              apy: apyMax,
+              token: token.toLowerCase(),
+              longPmrRate,
+              shortPmrRate,
+              price_change_rate: changeRate
+            }
           }
-        }
-      )
+        )
+      }
+      return []
+    },
+    {
+      refreshInterval: 6000
     }
-    return []
-  }, {
-    refreshInterval: 6000
-  })
+  )
 }
 
 export const usePairsExtData = (): number => {

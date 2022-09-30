04d5bcc04c62713762ca87f4af2fa810aba1b6a2
derivationstaff002 <fiuche@derify.org>
2022-09-30 16:30:00 +0800

    no message

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import React, { lazy, Suspense } from 'react'
 import { Switch, Route } from '@/components/common/Route'
 
+import { useInitialEffect } from '@/hooks/useInitialEffect'
+import AccessDeniedDialog from '@/components/common/Wallet/AccessDenied'
+
 import 'rc-dialog/assets/index.css'
 import 'rc-collapse/assets/index.css'
 import 'rc-tabs/assets/index.css'
 import 'rc-table/assets/index.css'
 
 import '@/style/style.scss'
-import { useInitialEffect } from '@/hooks/useInitialEffect'
 
 const WebEntry = lazy(() => import('@/pages/web'))
 
@@ -15,11 +17,14 @@ function App() {
   useInitialEffect()
 
   return (
-    <Suspense fallback={null}>
-      <Switch>
-        <Route path="/" component={WebEntry} />
-      </Switch>
-    </Suspense>
+    <>
+      <Suspense fallback={null}>
+        <Switch>
+          <Route path="/" component={WebEntry} />
+        </Switch>
+      </Suspense>
+      <AccessDeniedDialog visible />
+    </>
   )
 }
 

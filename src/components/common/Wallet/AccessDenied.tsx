0458d3d50e2ae453300a6e63c0215d9249b6e3da
derivationstaff002 <fiuche@derify.org>
2022-09-30 16:29:15 +0800

    no message

--- src/components/common/Wallet/AccessDenied.tsx
+++ src/components/common/Wallet/AccessDenied.tsx
@@ -8,10 +8,14 @@ interface Props {
 
 const AccessDeniedDialog: FC<Props> = ({ visible }) => {
   return (
-    <Dialog width="472px" visible={visible} title='Access Denied' closable={false}>
+    <Dialog width="472px" visible={visible} title="Access Denied" closable={false}>
       <div className="web-access-denied-dialog">
         <p className="web-access-denied-dialog-tips">
-          You are prohibited from using the Site in your current region due to violation of the <a href='https://derify.finance/terms' target='_blank'>Terms of Service</a>.
+          You are prohibited from using the Site in your current region due to violation of the{' '}
+          <a href="https://derify.finance/terms" target="_blank">
+            Terms of Service
+          </a>
+          .
         </p>
       </div>
     </Dialog>

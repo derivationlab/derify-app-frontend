8458d4e33c314fc90be25c9a3c19a7754d65df58
derivationstaff002 <fiuche@derify.org>
2022-11-17 13:43:52 +0800

    fix

--- src/config/env.ts
+++ src/config/env.ts
@@ -12,7 +12,7 @@ const urlTable: Record<string, { web: string }> = {
 
 const { NODE_ENV } = process.env
 export const getEnv = (): string => {
-  if (NODE_ENV === 'development') return 'dev'
+  if (NODE_ENV === 'development') return 'prod'
   return Object.keys(urlTable).find((i) => JSON.stringify(urlTable[i]).includes(window.location.origin)) ?? 'prod'
 }
 

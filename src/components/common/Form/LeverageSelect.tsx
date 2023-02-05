ca5b3fa985075f403e85d199f04d8ac931ea3c35
derivationstaff002 <fiuche@derify.org>
2023-02-05 22:37:37 +0800

    fix

--- src/components/common/Form/LeverageSelect.tsx
+++ src/components/common/Form/LeverageSelect.tsx
@@ -32,7 +32,7 @@ const LeverageSelect: FC<Props> = ({ value, onChange, className }) => {
         {value}x
       </div>
       <div className="web-leverage-select-stepper">
-        <Stepper value={multiple} min={1} onChange={setMultiple} suffix="X" input />
+        <Stepper value={multiple} min={1} onChange={setMultiple} suffix="X" max={75} input />
         <Slider value={multiple} onChange={setMultiple} suffix="X" marks={[1, 15, 30, 45, 60, 75]} />
         <Button full size="medium" onClick={onConfirm}>
           Confirm

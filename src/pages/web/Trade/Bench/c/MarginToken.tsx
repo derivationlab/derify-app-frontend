222d68a59b2ba3e8813d140a6b8ae51ad1293ba3
derivationstaff002 <fiuche@derify.org>
2023-03-24 14:47:20 +0800

    no message

--- src/pages/web/Trade/Bench/c/MarginToken.tsx
+++ src/pages/web/Trade/Bench/c/MarginToken.tsx
@@ -1,29 +1,65 @@
 import { useHistory } from 'react-router-dom'
 import { useTranslation } from 'react-i18next'
-import React, { FC, useMemo } from 'react'
+import React, { FC, useCallback, useEffect, useMemo, useReducer } from 'react'
 
-import { MARGIN_TOKENS } from '@/config/tokens'
+import { findToken, MARGIN_TOKENS } from '@/config/tokens'
 import { useMarginToken } from '@/store'
 import { MarginTokenKeys } from '@/typings'
 
 import { Select } from '@/components/common/Form'
 import Image from '@/components/common/Image'
+import { getMySpaceMarginTokenList } from '@/api'
+import { useAccount } from 'wagmi'
+import { reducer, stateInit } from '@/reducers/mySpace'
+import classNames from 'classnames'
 
 const MarginToken: FC = () => {
+  const [state, dispatch] = useReducer(reducer, stateInit)
+
   const history = useHistory()
 
   const { t } = useTranslation()
+  const { address } = useAccount()
 
   const marginToken = useMarginToken((state) => state.marginToken)
   const updateMarginToken = useMarginToken((state) => state.updateMarginToken)
 
   const marginOptions = useMemo(() => {
-    return MARGIN_TOKENS.map((t) => ({
+    return state.marginData.records.map((t) => ({
       value: t.symbol,
       label: t.symbol,
-      icon: `market/${t.symbol.toLowerCase()}.svg`
+      isOpen: !!t.open,
+      icon: findToken(t.symbol).icon
     }))
-  }, [])
+  }, [state.marginData])
+
+  const defaultMargin = useMemo(() => {
+    return {
+      value: marginToken,
+      label: marginToken,
+      isOpen: true,
+      icon: findToken(marginToken).icon
+    }
+  }, [marginToken])
+
+  // todo more margins loading...
+  const fetchData = useCallback(
+    async (index = 0) => {
+      if (address) {
+        const { data } = await getMySpaceMarginTokenList(address, index, 10)
+
+        dispatch({
+          type: 'SET_MARGIN_DAT',
+          payload: { records: data?.records ?? [], totalItems: data?.totalItems ?? 0, isLoaded: false }
+        })
+      }
+    },
+    [address]
+  )
+
+  useEffect(() => {
+    if (address) void fetchData()
+  }, [address])
 
   return (
     <div className="web-trade-bench-margin">
@@ -37,17 +73,17 @@ const MarginToken: FC = () => {
           history.push(`/${v}/trade`)
         }}
         renderer={(props) => (
-          <div className="web-select-options-item">
-            {props.icon && <Image src={props.icon} />}
-            {props.label}
+          <div className={classNames('web-select-options-item', { close: !props.isOpen })}>
+            {props?.icon && <Image src={props?.icon} />}
+            {props?.label}
           </div>
         )}
         className="web-trade-bench-margin-select"
         objOptions={marginOptions}
-        labelRenderer={(props) => (
+        labelRenderer={() => (
           <div className="web-dashboard-add-grant-margin-label">
-            {props.icon && <Image src={props.icon} />}
-            <span>{props.label}</span>
+            <Image src={defaultMargin.icon} />
+            <span>{defaultMargin.label}</span>
           </div>
         )}
         filterPlaceholder="Search name or contract address..."

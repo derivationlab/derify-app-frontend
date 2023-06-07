2846fef00b5efac9ad703b5a135c808d08930c5b
derivationstaff002 <fiuche@derify.org>
2023-06-07 15:49:12 +0800

    no message

--- src/hooks/useMarginOperation.ts
+++ src/hooks/useMarginOperation.ts
@@ -1,9 +1,37 @@
+import type { Contract } from '@ethersproject/contracts'
+import type { Signer } from 'ethers'
+
 import { marginToken } from '@/store'
 import { TSigner } from '@/typings'
 import { allowanceApprove } from '@/utils/allowanceApprove'
 import { getDerifyExchangeContract } from '@/utils/contractHelpers'
 import { estimateGas } from '@/utils/estimateGas'
 import { inputParameterConversion } from '@/utils/tools'
+
+const depositMarginForWBNB = async (contract: Contract, amount: string): Promise<boolean> => {
+  const _amount = inputParameterConversion(amount, 18)
+  const gasLimit = await estimateGas(contract, 'deposit', [_amount, { value: _amount }])
+  const res = await contract.deposit(_amount, { gasLimit, value: _amount })
+  const receipt = await res.wait()
+  return receipt.status
+}
+
+const depositMarginForNormal = async (
+  contract: Contract,
+  exchange: string,
+  amount: string,
+  margin: string,
+  signer: Signer
+): Promise<boolean> => {
+  const _amount1 = inputParameterConversion(amount, 8)
+  const _amount2 = inputParameterConversion(amount, 18)
+  const approve = await allowanceApprove(signer, exchange, margin, _amount2)
+  if (!approve) return false
+  const gasLimit = await estimateGas(contract, 'deposit', [_amount1])
+  const res = await contract.deposit(_amount1, { gasLimit })
+  const receipt = await res.wait()
+  return receipt.status
+}
 
 export const useMarginOperation = () => {
   const deposit = async (
@@ -13,23 +41,15 @@ export const useMarginOperation = () => {
     signer?: TSigner
   ): Promise<boolean> => {
     if (!signer) return false
-    let approve = false
-    const c = getDerifyExchangeContract(exchange, signer)
-    const _amount1 = inputParameterConversion(amount, margin.symbol === 'BNB' ? 18 : 8)
-    const _amount2 = inputParameterConversion(amount, 18)
-
+    const contract = getDerifyExchangeContract(exchange, signer)
     try {
-      if (margin.symbol === 'BNB') approve = true
-      else {
-        approve = await allowanceApprove(signer, exchange, margin.address, _amount2)
+      if (margin.symbol === 'BNB') {
+        const status = await depositMarginForWBNB(contract, amount)
+        return status
+      } else {
+        const status = await depositMarginForNormal(contract, exchange, amount, margin.address, signer)
+        return status
       }
-
-      if (!approve) return false
-
-      const gasLimit = await estimateGas(c, 'deposit', [_amount1])
-      const res = await c.deposit(_amount1, { gasLimit })
-      const receipt = await res.wait()
-      return receipt.status
     } catch (e) {
       console.info(e)
       return false

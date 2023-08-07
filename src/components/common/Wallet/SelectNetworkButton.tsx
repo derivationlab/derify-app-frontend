044b12d1434fe8df3706d643230b58b3d90fcbc7
derivationstaff002 <fiuche@derify.org>
2023-08-07 12:52:04 +0800

    fix

--- src/components/common/Wallet/SelectNetworkButton.tsx
+++ src/components/common/Wallet/SelectNetworkButton.tsx
@@ -1,14 +1,11 @@
 import classNames from 'classnames'
-import { Chain, useNetwork, useSwitchNetwork } from 'wagmi'
 
-import React, { FC, useState, useRef, useEffect, useCallback } from 'react'
+import React, { FC, useState, useRef } from 'react'
 import { useTranslation } from 'react-i18next'
 import { useClickAway } from 'react-use'
 
 import Button from '@/components/common/Button'
 import Image from '@/components/common/Image'
-import { useRpcNodeStore } from '@/store'
-import { RpcNodeState } from '@/store/types'
 import { ChainId } from '@/typings'
 
 const networks = {
@@ -25,40 +22,18 @@ const networks = {
 const SelectNetworkButton: FC = () => {
   const ref = useRef(null)
   const { t } = useTranslation()
-  const { chain } = useNetwork()
-  const { switchNetwork, isLoading } = useSwitchNetwork()
-
-  const fetchRpc = useRpcNodeStore((state: RpcNodeState) => state.fetch)
 
   const [visible, setVisible] = useState<boolean>(false)
-
-  const selectNetwork = useCallback(
-    (network: Chain) => {
-      if (chain?.id !== network.id) switchNetwork?.(network.id)
-      setVisible(false)
-    },
-    [chain]
-  )
 
   useClickAway(ref, () => {
     setVisible(false)
   })
 
-  useEffect(() => {
-    if (chain?.id === ChainId.TESTNET && !isLoading) void fetchRpc(chain?.id)
-  }, [chain?.id, isLoading])
-
   return (
     <div className="web-select-network-button" ref={ref}>
-      <Button size="mini" outline loading={isLoading} onClick={() => setVisible(!visible)}>
-        {chain?.id === ChainId.TESTNET ? (
-          <>
-            <Image src="icon/bnb.svg" />
-            {isLoading ? 'Network Switching ...' : networks[chain?.id as ChainId]?.name}
-          </>
-        ) : (
-          'Select Network'
-        )}
+      <Button size="mini" outline onClick={() => setVisible(!visible)}>
+        <Image src="icon/bnb.svg" />
+        {networks[ChainId.MAINNET].name}
       </Button>
       <div className={classNames('web-select-network-menu', { show: visible })}>
         <h3>{t('Nav.CW.SelectNetwork', 'Select a network')}</h3>

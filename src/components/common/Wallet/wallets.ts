5fadcb7fddaa7465e6a3488d8f8a06b8161cf94e
derivationstaff002 <fiuche@derify.org>
2023-08-09 11:50:28 +0800

    fix

--- src/components/common/Wallet/wallets.ts
+++ src/components/common/Wallet/wallets.ts
@@ -10,16 +10,31 @@ export type Wallet<T = ConnectorIds> = {
   downloadLink?: string
 }
 
+const isMetamaskInstalled = () => {
+  if (typeof window === 'undefined') return false
+  if (window.ethereum?.isMetaMask) return true
+  return !!window.ethereum?.providers?.some((p) => p.isMetaMask)
+}
+
 const wallets: Wallet[] = [
   {
     id: 'metaMask',
     icon: 'metamask.svg',
     title: 'MetaMask',
     get installed() {
-      return typeof window !== 'undefined' && !!window.ethereum?.isMetaMask && metaMaskConnector.ready
+      return isMetamaskInstalled()
     },
     connectorId: ConnectorIds.MetaMask,
     downloadLink: 'https://metamask.io/download/'
+  },
+  {
+    id: 'tokenPocket',
+    title: 'TokenPocket',
+    icon: `token-pocket.svg`,
+    connectorId: ConnectorIds.Injected,
+    get installed() {
+      return typeof window !== 'undefined' && !!(window.ethereum?.isTokenPocket)
+    },
   },
   {
     id: 'coinbaseWallet',

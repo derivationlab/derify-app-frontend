586301db679a645e840abe88847ca5429d637204
derivationstaff002 <fiuche@derify.org>
2023-08-10 16:35:16 +0800

    fix

--- src/config/contracts.ts
+++ src/config/contracts.ts
@@ -6,6 +6,10 @@ export const contracts: { [key in ContractKeys]: Contract } = {
     [ChainId.MAINNET]: '0xfF6FD90A470Aaa0c1B8A54681746b07AcdFedc9B',
     [ChainId.TESTNET]: '0x8F3273Fb89B075b1645095ABaC6ed17B2d4Bc576'
   }),
+  derifyPool: new Contract('DerifyPool', {
+    [ChainId.MAINNET]: '0x755BAEeeE6F506278C2BD692F89e1aa25FE3f394',
+    [ChainId.TESTNET]: '0x4922D9769197d3FE27c3c6c4372D3fb76C49B27E'
+  }),
   derifyProtocol: new Contract('DerifyProtocol', {
     [ChainId.MAINNET]: '0x2e70e1C2B3660B91E07DEa476F36945aDFe1e4A1',
     [ChainId.TESTNET]: '0x3F5902E6257CeA812B8781cFC45Ec56317C44e30'

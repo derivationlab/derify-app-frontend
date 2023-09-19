455480b01aab70918b7ea7876bcb154314215fff
derivationstaff002 <fiuche@derify.org>
2023-09-19 22:29:22 +0800

    fix: logic

--- src/hooks/useConsultant.ts
+++ src/hooks/useConsultant.ts
@@ -6,12 +6,13 @@ import { TSigner } from '@/typings'
 import { allowanceApprove } from '@/utils/allowanceApprove'
 import { getConsultantContract } from '@/utils/contractHelpers'
 import { inputParameterConversion, safeInterceptionValues } from '@/utils/tools'
+import { advisorConfig } from '@/pages/web/consultant/Lock'
 
 export const useConsultant = () => {
   const addInsurance = async (signer: TSigner): Promise<boolean> => {
     if (!signer) return false
     const c = getConsultantContract(signer)
-    const amount = inputParameterConversion(100000, PLATFORM_TOKEN.precision)
+    const amount = inputParameterConversion(advisorConfig.amount, PLATFORM_TOKEN.precision)
     try {
       const approve = await allowanceApprove(
         signer,

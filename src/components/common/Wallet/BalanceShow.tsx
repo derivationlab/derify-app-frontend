0beedfa069f05d3a758bf8bd162bffdebc3794f5
derivationstaff002 <fiuche@derify.org>
2023-12-08 16:55:10 +0800

    fix: update

--- src/components/common/Wallet/BalanceShow.tsx
+++ src/components/common/Wallet/BalanceShow.tsx
@@ -24,17 +24,28 @@ const BalanceShow: FC<Props> = ({ unit, rule, value, percent = false, decimal = 
     const output = percent ? bnMul(value, 100) : value
     const absValue = Math.abs(Number(output))
     const isMillion = absValue >= million
-    const formatRule = rule || (isMillion ? `0,0.${padEnd} a` : `0,0.${padEnd}`)
+    const formatRule = rule || (isMillion ? `0,0.${padEnd}a` : `0,0.${padEnd}`)
     if (absValue < limit) split = Number(output).toFixed(decimal).split('.')
     else split = numeral(output).format(formatRule).split('.')
     return [isMillion, ...split]
   }, [value, percent])
 
+  const _dec = useMemo(() => {
+    const a = dec.replace(/k|m|b|t/gi, '')
+    const b = dec.replace(/[0-9]+/g, '')
+    return (
+      <>
+        .<u>{a}</u>
+        <u>{b}</u>
+      </>
+    )
+  }, [dec])
+
   return (
     <div className={cls('web-balance-show', classNames, { million: isMillion })}>
       <strong>{int ?? 0}</strong>
       <small>
-        {dec && `.${dec}`}
+        {dec && _dec}
         {percent ? '%' : ''}
       </small>
       {unit && <span>{unit}</span>}

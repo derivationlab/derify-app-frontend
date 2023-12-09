de1c866253be67410e0d7a335e4dd82d74566fc3
derivationstaff002 <fiuche@derify.org>
2023-12-09 22:39:53 +0800

    fix: update

--- src/config/contracts.ts
+++ src/config/contracts.ts
@@ -4,15 +4,15 @@ import { ChainId, ContractKeys } from '@/typings'
 export const contracts: { [key in ContractKeys]: Contract } = {
   derifyProtocol: new Contract('DerifyProtocol', {
     [ChainId.MAINNET]: '0x2e70e1C2B3660B91E07DEa476F36945aDFe1e4A1',
-    [ChainId.TESTNET]: '0x1b704a8ee3591Ec26fa235B232983563BF904Cc1'
+    [ChainId.TESTNET]: '0x88972089Dab83A4541E1d9E07b1F6E0e99db119C'
   }),
   derifyConsultant: new Contract('DerifyConsultant', {
     [ChainId.MAINNET]: '0x0000000000000000000000000000000000000000',
-    [ChainId.TESTNET]: '0xD8C3DFC921c4464DEFCC0EDB76cf361805d252C7'
+    [ChainId.TESTNET]: '0x70c74940097e7c1946263a0D471Bf47AF97a5C5A'
   }),
   derifyApply: new Contract('DerifyApply', {
     [ChainId.MAINNET]: '0x0000000000000000000000000000000000000000',
-    [ChainId.TESTNET]: '0xBcEbC6177ab893574f066aEeED608f8E9aE9274d'
+    [ChainId.TESTNET]: '0x2665bF41127021FC3542676621E70B620278E260'
   }),
   multicall: new Contract('multicall', {
     [ChainId.MAINNET]: '0xfF6FD90A470Aaa0c1B8A54681746b07AcdFedc9B',

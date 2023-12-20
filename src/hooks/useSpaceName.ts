63d480aa0325e391edac1aa3531f8486a2c4ab57
derivationstaff002 <fiuche@derify.org>
2023-12-20 15:21:31 +0800

    fix: update

--- src/hooks/useSpaceName.ts
+++ src/hooks/useSpaceName.ts
@@ -1,27 +1,21 @@
-import { createWeb3Name } from '@web3-name-sdk/core'
 import { useAccount } from 'wagmi'
 
 import { useEffect, useState } from 'react'
-
-import { INIT_RPC_URL } from '@/utils/wagmiConfig'
+import { getAddressSpaceName } from 'derify-apis-staging'
+import { Rec } from '@/typings'
 
 export const useSpaceName = () => {
-  const [web3Name] = useState(() => createWeb3Name())
-
   const { address } = useAccount()
 
   const [spaceName, setSpaceName] = useState<string | null>(null)
 
   useEffect(() => {
-    const func = async (address: string, web3Name: any) => {
-      const name = await web3Name.getDomainName({
-        address,
-        rpcUrl: INIT_RPC_URL()
-      })
+    const func = async (address: string) => {
+      const { name } = await getAddressSpaceName<Rec>(address,'bnb')
       setSpaceName(name)
     }
-    if (address && web3Name) void func(address, web3Name)
-  }, [address, web3Name])
+    if (address) void func(address)
+  }, [address])
 
   return {
     spaceName
